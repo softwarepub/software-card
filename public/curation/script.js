54263c8f12106d5a263f6a39234eeaacb2c06220
Kernchen, Sophie <sophie.kernchen@web.de>
2025-10-07 10:28:21 +0200

    Connect views

--- public/curation/script.js
+++ public/curation/script.js
@@ -16,4 +16,4 @@ drawRadar(ctx, canvas.offsetWidth/2,canvas.offsetHeight/2, [15,15,10,15,15,12], 
 drawRadar(ctx, canvas.offsetWidth/2,canvas.offsetHeight/2, [20,20,9,10,20,15], "blue", "rgba(94, 148, 215, 0.34)");
 
 //View for Curation
-displayJSON("https://raw.githubusercontent.com/SKernchen/SoftwareCaRD-test/refs/heads/main/.hermes/process/hermes.json");
+displayJSON("../.hermes/process/hermes.json");
