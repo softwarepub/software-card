a6fd3eb6b9e8e714c4536409a6a380992a98b953
Kernchen, Sophie <sophie.kernchen@web.de>
2025-10-28 14:35:48 +0100

    Add new categorie to load the latest job

--- public/callback/main.js
+++ public/callback/main.js
@@ -13,10 +13,15 @@ window.onload = async function () {
     }
 
     const gitLabProjectId = parameters.get("gitlab_project_id");
+    
+    const get_latest = parameters.get("latest");
+
     const gitLabPipelineId = parameters.get("gitlab_pipeline_id");
     const gitLabJobId = parameters.get("gitlab_job_id");
 
+    if(get_latest != 1){
     await registerPipeline(gitLabProjectId, gitLabPipelineId, gitLabJobId);
+    }
 
     if (showDebugInformation) {
         await new Promise(r => setTimeout(r, 5000));
@@ -24,11 +29,36 @@ window.onload = async function () {
 
     const token = localStorage.getItem("gitlab-api-token");
     if (token) {
-        window.location = "/software-card/dashboard/";
+        if(get_latest==1){
+            await latest(gitLabProjectId, token);
+        }else{
+        window.location = "../dashboard/";
+        }
         return;
     } else {
         alert("Please set up the GitLab connection, then go to the dashboard!");
-        window.location = "/software-card/gitlab-setup/";
+        window.location = "../gitlab-setup/";
         return;
     }
+
+    
+}
+
+async function latest(projectId, token) {
+    const jobResponse = await fetch(
+        `https://codebase.helmholtz.cloud/api/v4/projects/${projectId}/jobs/`,
+        { headers: { "Content-Type": "application/json", "PRIVATE-TOKEN": token } }
+    );
+
+    if (!jobResponse.ok) {
+        alert("Fetching pipeline failed");
+        return;
+    }
+
+    const jobData = await jobResponse.json();
+    const jobId = jobData[0]["id"];
+    const pipelineId = jobData[0]["pipeline"]["id"];
+    
+    window.location = `../callback?gitlab_project_id=${projectId}&gitlab_pipeline_id=${pipelineId}&gitlab_job_id=${jobId}&latest=2`;
+    //gitlab_pipeline_id=618554&gitlab_job_id=2513432
 }
